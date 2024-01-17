d64eadac6e361c3544e844e2c034a0b3aa741e1f
Fahime <fahim.mohammad.siam@gmail.com>
2024-01-18 00:05:53 +0600

    Pulling the code from development branch

--- src/Pages/Home/SuccessShowcase/SuccessShowcase.jsx
+++ src/Pages/Home/SuccessShowcase/SuccessShowcase.jsx
@@ -1,15 +1,15 @@
+// @ts-nocheck
 import Container from "../../../Component/UI/Container";
 import Heading from "../../../Component/UI/Heading";
 import Description from "../../../Component/UI/Description";
 
-
 const SuccessShowcase = () => {
-    return (
-        <Container>
-        <Heading>Success showcase</Heading>
-        <Description>description</Description>
+  return (
+    <Container>
+      <Heading>Success showcase</Heading>
+      <Description>description</Description>
     </Container>
-    );
+  );
 };
 
 export default SuccessShowcase;

0a79a3df67572dd48d0ea51f04528f0c30a20487
Digontha <digonthodj@gmail.com>
2024-01-17 14:07:38 +0600

    create input for email and also select for domain

--- src/Pages/Home/FreeTrial/FreeTrial.jsx
+++ src/Pages/Home/FreeTrial/FreeTrial.jsx
@@ -2,6 +2,7 @@ import Container from "../../../Component/UI/Container";
 import Heading from "../../../Component/UI/Heading";
 import Description from "../../../Component/UI/Description";
 import image from "../../../assets/FreeTrialSectionImages/Domain-free-trial.png"
+import { MdEmail } from "react-icons/md";
 
 const FreeTrial = () => {
     return (
@@ -12,40 +13,53 @@ const FreeTrial = () => {
                 <Description>description</Description>
             </Container>
 
-{/* Free Trial Section Start */}
+            {/* Free Trial Section Start */}
 
-<section className="flex lg:flex-row flex-col items-center justify-between lg:px-[15%]">
+            <section className="flex lg:flex-row flex-col items-center justify-between lg:bg-slate-100 lg:px-[15%]  lg:h-[600px]">
 
-<div>
-    <img src={image} className="lg:w-[750px]" alt="" />
-</div>
 
-<div className=" h-screen flex items-center justify-center">
 
-    <div className="max-w-md p-8 bg-white shadow-md rounded-md">
+                <div className="lg:w-[750px] space-y-10" >
+                    <img src={image} alt="" />
+                </div>
 
-        <h1 className="text-4xl font-bold text-blue-500 mb-6">Free Trial Application</h1>
 
-        <form className="mb-4">
-            <label  className="block text-gray-700 text-sm font-semibold mb-2">Email Address:</label>
-            <input type="email" id="email" name="email" className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" required/>
-        </form>
+                <div className=" h-screen flex items-center justify-center">
 
-        <p className="text-gray-700 mb-4">
-            Sign up for a free trial and explore our amazing features!.
-        </p>
+                    <div className="max-w-md p-8 bg-white shadow-md rounded-md">
 
-        <button type="submit" className="bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
-            Apply for Free Trial
-        </button>
+                        <h1 className="lg:text-4xl text-3xl font-bold text-blue-500 mb-6">Free Trial Application</h1>
 
-    </div>
+                        <form className="mb-4 ">
+                            <label className=" text-gray-700 text-sm font-semibold mb-2 flex items-center gap-1"><MdEmail></MdEmail>Email Address</label>
+                            
+                            <input type="email" id="email" name="email" className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" required />
+                          <hr className="mt-5" />
+                            
+                            <select name="Domain" className="select p-0 appearance-none w-full border-none outline-none text-black text-sm font-semibold">
+                                <option disabled selected>Select Domain</option>
+                                <option value="com">com</option>
+                                <option value="info">info</option>
+                                <option value="net">net</option>
+                            </select>
 
-</div>
+                        </form>
 
-</section>
+                        <p className="text-gray-700 mb-4 text-[12px] font-thin ">
+                            Sign up for 7 days a free trial and explore our amazing features! We understand the importance of finding the perfect domain to elevate your online presence.
+                        </p>
 
-{/* Free Trial Section Finish */}
+                        <button type="submit" className="bg-blue-500 btn text-white p-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300">
+                            Apply for Free Trial
+                        </button>
+
+                    </div>
+
+                </div>
+
+            </section>
+
+            {/* Free Trial Section Finish */}
 
 
         </>

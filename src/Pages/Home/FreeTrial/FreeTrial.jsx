4d7ff68d5d010a82699a2946e0f92d9449b9fce2
Digontha <digonthodj@gmail.com>
2024-01-18 15:21:08 +0600

    work on theme

--- src/Pages/Home/FreeTrial/FreeTrial.jsx
+++ src/Pages/Home/FreeTrial/FreeTrial.jsx
@@ -15,7 +15,7 @@ const FreeTrial = () => {
 
             {/* Free Trial Section Start */}
 
-            <section className="flex lg:flex-row flex-col items-center justify-between lg:bg-slate-100 lg:px-[15%]  lg:h-[600px]">
+            <section className="flex lg:flex-row flex-col items-center justify-between lg:bg-slate-100 rounded-md dark:bg-black lg:px-[15%]  lg:h-[600px]">
 
 
 
@@ -26,15 +26,15 @@ const FreeTrial = () => {
 
                 <div className=" h-screen flex items-center justify-center">
 
-                    <div className="max-w-md p-8 bg-white shadow-md rounded-md">
+                    <div className="max-w-md p-8 dark:bg-[#191919] bg-white shadow-md rounded-md">
 
                         <h1 className="lg:text-4xl text-3xl font-bold text-blue-500 mb-6">Free Trial Application</h1>
 
                         <form className="mb-4 ">
-                            <label className=" text-gray-700 text-sm font-semibold mb-2 flex items-center gap-1"><MdEmail></MdEmail>Email Address</label>
+                            <label className=" text-gray-700 dark:text-white text-sm font-semibold mb-2 flex items-center gap-1"><MdEmail></MdEmail>Email Address</label>
                             
                             <input type="email" id="email" name="email" className="w-full p-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500" required />
-                          <hr className="mt-5" />
+                          <hr className="my-5" />
                             
                             <select name="Domain" className="select p-0 appearance-none w-full border-none outline-none text-black text-sm font-semibold">
                                 <option disabled selected>Select Domain</option>
@@ -45,7 +45,7 @@ const FreeTrial = () => {
 
                         </form>
 
-                        <p className="text-gray-700 mb-4 text-[12px] font-thin ">
+                        <p className="text-gray-700 dark:text-white mb-4 text-[12px] font-thin ">
                             Sign up for 7 days a free trial and explore our amazing features! We understand the importance of finding the perfect domain to elevate your online presence.
                         </p>
 

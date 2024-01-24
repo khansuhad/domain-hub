377b3f05797376d3027bc935e2df34fb6db04f31
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-01-24 22:39:42 +0600

    add private route

---
+++ src/Routers/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from "prop-types";
+import UseAuth from "../Hock/UseAuth";
+import Loading from "../Component/Loading/Loading";
+
+const PrivateRoute = ({ children }) => {
+  const { user, isLoading } = UseAuth();
+  const location = useLocation();
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+};
+
+export default PrivateRoute;
+
+PrivateRoute.propTypes = {
+  children: PropTypes.node,
+};

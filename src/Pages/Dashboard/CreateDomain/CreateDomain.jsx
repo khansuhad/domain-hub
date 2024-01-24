f7d4bbdf1d1cd973f265425420d12ee77d99fbda
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-01-24 17:34:58 +0600

    made all Domain page

--- src/Pages/Dashboard/CreateDomain/CreateDomain.jsx
+++ src/Pages/Dashboard/CreateDomain/CreateDomain.jsx
@@ -1,10 +1,8 @@
-// import Swal from "sweetalert2";
 import DomainForm from "../../../Component/Shared/domainForm/domainForm";
-// import useAxiosPublic from "../../../Hock/useAxiosPublic";
+
 
 
 const CreateDomain =() => {
-    // const useAxios = useAxiosPublic();
     return (
         <div className="md:my-40 md:mx-0 lg:mx-[5%]">
            <DomainForm></DomainForm>

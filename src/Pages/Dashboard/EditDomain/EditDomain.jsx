cf598fe0c0ea97dcf49ed96ff70704821eae638f
Sharif-codes <sharifxenjia@gmail.com>
2024-01-26 00:46:35 +0600

    update domain completed

--- src/Pages/Dashboard/EditDomain/EditDomain.jsx
+++ src/Pages/Dashboard/EditDomain/EditDomain.jsx
@@ -1,20 +1,30 @@
 import { useSelector } from "react-redux";
+import useAxiosPublic from "../../../Hock/useAxiosPublic";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 
 const EditDomain = () => {
+    const navigate= useNavigate()
+    const axiosPublic = useAxiosPublic();
     const updateDomain = useSelector((state) => state.domain.domain)
     console.log(updateDomain)
-    const handleDomain = e => {
+    const handleUpdate = async e => {
         e.preventDefault();
         const form = e.target;
         const name = form.tld.value;
         const price = form.price.value;
         const category = form.category.value;
-        const trial = form.trial.value;
         const description = form.description.value;
 
-        const cartItem = { name, price, category, description, trial }
-        console.log(cartItem);
+        const updatedData = { name, price, category, description }
+        console.log(updatedData);
+        const res=await axiosPublic.patch(`/domain/${updateDomain._id}`,updatedData)
+        if(res.statusText==="OK")
+        {
+            toast.success("Domain updated successfully")
+            navigate("/dashboard/all-domains")
+        }
     }
 
     return (
@@ -25,20 +35,20 @@ const EditDomain = () => {
                 <h3 className="text-center text-xl md:text-2xl font-bold  text-white">Update Domain</h3>
 
 
-                <form className="mt-10" onSubmit={handleDomain}>
+                <form className="mt-10" onSubmit={handleUpdate}>
 
-                    <div className="m-5 md:flex gap-5 justify-center">
-                        <div className="form-control md:w-1/2 ">
+                    <div className="m-5 gap-5 justify-center">
+                        <div className="form-control w-full ">
                             <label className="label">
                                 <span className="label-text font-bold">TLD Name</span>
                             </label>
                             <label className="input-group">
 
-                                <input type="text" name="tld" disabled defaultValue={updateDomain.name} placeholder="TLD name" className="input input-bordered w-full" />
+                                <input type="text" name="tld" defaultValue={updateDomain.name} placeholder="TLD name" className="input input-bordered w-full" />
                             </label>
                         </div>
 
-                        <div className="form-control md:w-1/2">
+                        <div className="form-control w-full">
                             <label className="label">
                                 <span className="label-text font-bold">Price</span>
                             </label>
@@ -48,36 +58,6 @@ const EditDomain = () => {
                             </label>
                         </div>
                     </div>
-
-                    <div className="m-5 md:flex gap-5 justify-center">
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text font-bold"> Description</span>
-                            </label>
-                            <label className="input-group">
-
-                                <input type="text" name="description" defaultValue={updateDomain.description} placeholder="Description" className="input input-bordered w-full" />
-                            </label>
-                        </div>
-
-                        <div className="form-control md:w-1/2">
-                            <label className="label">
-                                <span className="label-text font-bold">Free Trial (Month)</span>
-                            </label>
-                            <label className="input-group">
-
-                            <select name="trial" className="select select-bordered w-full" defaultValue={updateDomain.trial}>
-
-                            <option value="1">1</option>
-                            <option value="2">2</option>
-                            <option value="3">3</option>
-    
-                            {/* Add more categories as needed */}
-                        </select>
-                            </label>
-                        </div>
-                    </div>
-
                     <div className="m-5 md:flex gap-5 justify-center">
                         <div className="form-control w-full">
                             <label className="label">
@@ -85,27 +65,35 @@ const EditDomain = () => {
                             </label>
                             <label className="input-group">
                                 <select name="category" className="select select-bordered w-full" defaultValue={updateDomain.category}>
-
-                                    <option value="education">health</option>
-                                    <option value="coding">commerce</option>
-                                    <option value="science">education</option>
-                                    <option value="science">government</option>
-                                    <option value="science">education</option>
-                                    <option value="science">sport</option>
-                                    <option value="science">technology</option>
+                                    <option value="health">health</option>
+                                    <option value="commerce">commerce</option>
+                                    <option value="education">education</option>
+                                    <option value="government">government</option>
+                                    <option value="education">education</option>
+                                    <option value="sport">sport</option>
+                                    <option value="technology">technology</option>
                                     {/* Add more categories as needed */}
                                 </select>
                             </label>
                         </div>
-
                     </div>
+                    <div className="m-5 md:flex gap-5 justify-center">
+                        <div className="form-control w-full">
+                            <label className="label">
+                                <span className="label-text font-bold"> Description</span>
+                            </label>
+                            <label className="input-group">
 
+                                {/* <input type="text" name="description" placeholder="Description" className="input input-bordered w-full" /> */}
+
+                                <textarea className="w-full" defaultValue={updateDomain.description} name="description" id="" cols="30" rows="10"></textarea>
+                            </label>
+                        </div>
+                    </div>
                     <div className="m-5  gap-5 justify-center mb-5">
                         <input type="submit" value="Update Domain" className="text-white btn btn-block hover:bg-thirdColor bg-fourthColor" />
                     </div>
-
                 </form>
-
             </div>
         </div>
 

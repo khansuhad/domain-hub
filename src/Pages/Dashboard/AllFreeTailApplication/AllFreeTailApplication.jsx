9e1b3a971d2aaa768b6547ba6a9d4d85fb93bccf
Digontha <digonthodj@gmail.com>
2024-01-26 01:25:52 +0600

    done

--- src/Pages/Dashboard/AllFreeTailApplication/AllFreeTailApplication.jsx
+++ src/Pages/Dashboard/AllFreeTailApplication/AllFreeTailApplication.jsx
@@ -1,12 +1,41 @@
 
+import axios from "axios";
 import useFreeTrial from "../../../Hock/useFreeTrial";
+import swal from "sweetalert";
+
 
 
 const AllFreeTailApplication = () => {
-    const [freeTrialUsers] = useFreeTrial();
+    const [freeTrialUsers,,refetch] = useFreeTrial();
     console.log(freeTrialUsers);
-
     
+
+    const handleApprove =(email)=>{
+      axios.put(`http://localhost:5000/freeTrialUsers?email=${email}`)
+      .then(res=>{
+        console.log(res.data);
+        if(res.data.modifiedCount>0){
+            swal("Application approve", "sent", "success");
+            refetch()
+        }
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
+    const handleDismiss =(email)=>{
+      axios.patch(`http://localhost:5000/freeTrialUsers?email=${email}`)
+      .then(res=>{
+        console.log(res.data);
+        if(res.data.modifiedCount>0){
+            swal("Application dismiss", "sent", "success");
+            refetch()
+        }
+      })
+      .catch(err=>{
+        console.log(err);
+      })
+    }
     
     return (
         <>
@@ -58,10 +87,12 @@ const AllFreeTailApplication = () => {
                                         </p>
                                     </td>
                                     <td className="p-4 border-b border-blue-gray-50 flex gap-3">
-                                        <p className="block cursor-pointer bg-red-400 p-2 rounded-md font-sans text-sm antialiased font-semibold leading-normal text-blue-gray-900">
+                                        {users.approve? <p onClick={()=>{handleApprove(users.email)}} className="block cursor-pointer bg-red-400 p-2 rounded-md font-sans text-sm antialiased font-semibold leading-normal text-blue-gray-900">
+                                            Approved
+                                        </p>:  <p onClick={()=>{handleApprove(users.email)}} className="block cursor-pointer bg-red-400 p-2 rounded-md font-sans text-sm antialiased font-semibold leading-normal text-blue-gray-900">
                                             Approve
-                                        </p>                                                                            
-                                        <p className="block cursor-pointer bg-yellow-400 p-2 rounded-md  font-sans text-sm antialiased font-semibold leading-normal text-blue-gray-900">
+                                        </p>}                                                                           
+                                        <p onClick={()=>{handleDismiss(users.email)}} className="block cursor-pointer bg-yellow-400 p-2 rounded-md  font-sans text-sm antialiased font-semibold leading-normal text-blue-gray-900">
                                             Dismiss
                                         </p>                                                                            
                                        

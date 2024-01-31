4442bd130f74bbde91a4838ac2b2190d868ad13f
Digontha <digonthodj@gmail.com>
2024-01-31 13:26:28 +0600

    create table for free trial

--- src/Hock/useAxiosSecure.jsx
+++ src/Hock/useAxiosSecure.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosSecure = axios.create({
-  // baseURL: "https://localhost:5000",
+  // baseURL: "http://localhost:5000",
   baseURL: " https://domain-hub-server-side.vercel.app",
  
 

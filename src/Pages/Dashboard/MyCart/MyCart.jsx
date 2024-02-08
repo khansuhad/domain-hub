3831033274187e2cc43b112eacd955b93707cca9
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-02-08 21:53:05 +0600

    price,discount,total price bug fix

--- src/Pages/Dashboard/MyCart/MyCart.jsx
+++ src/Pages/Dashboard/MyCart/MyCart.jsx
@@ -88,6 +88,7 @@ const MyCart = () => {
       }
     });
   };
+  console.log(totalPrice);
   return (
     <div className="rounded-lg text-white dark:bg-black min-h-screen bg-firstColor ">
       <div className="flex justify-between bg-gradient-to-tr from-[#13104e] to-[#0193e1] ">
@@ -130,7 +131,8 @@ const MyCart = () => {
                     <th>{index + 1}</th>
 
                     <td>{cartItem.name}</td>
-                    <td>${(parseFloat(cartItem.price) * selectedTimes[cartItem._id]).toFixed(2)}</td>
+                    <td>{selectedTimes[cartItem._id] ? (parseFloat(cartItem.price) * selectedTimes[cartItem._id]).toFixed(2) : "0.00"} $</td>
+
                     <td>
                       {/* Dropdown for selecting the number of years */}
                       <select
@@ -187,28 +189,18 @@ const MyCart = () => {
           <div className="bg-fourthColor w-full p-2 leading-10 rounded">
             <div className="flex justify-between">
               <p className="font-bold"> Total Price: </p>
-              <p> {parseFloat(totalPrice).toFixed(2)} $</p>
+              {/* total price */}
+              <p>{isNaN(totalPrice) ? "0.00" : parseFloat(totalPrice).toFixed(2)} $</p>
             </div>
             <div className="flex justify-between">
               <p className="font-bold"> Discount: </p>
-              <p>
-                {" "}
-                -
-                {(parseFloat(totalPrice) * (discountPercentage / 100)).toFixed(
-                  2
-                )}{" "}
-                $
-              </p>
+              {/* Discount price */}
+              <p>-{isNaN(totalPrice) || isNaN(discountPercentage) ? '0.00' : (parseFloat(totalPrice) * (discountPercentage / 100)).toFixed(2)} $</p>
             </div>
             <div className="flex justify-between">
               <p className="font-bold"> Total: </p>
-              <p className="font-bold">
-                {(
-                  parseFloat(totalPrice) -
-                  totalPrice * (discountPercentage / 100)
-                ).toFixed(2)}{" "}
-                $
-              </p>
+              {/* total price for payment */}
+              <p className="font-bold"> {!isNaN(totalPrice) && !isNaN(discountPercentage) ? (parseFloat(totalPrice) - totalPrice * (discountPercentage / 100)).toFixed(2) : '0.00'} $</p>
             </div>
 
             {totalPrice < 100 ? (

40fe64865f639972aabff7adbff4bc9a08fa9fa2
abubakarseddek1999 <abubakarseddek1999@gmail.com>
2024-02-13 01:18:33 +0600

    add banner in searchDomain page

--- src/Pages/SearchDomainPage/SearchingDomain.jsx
+++ src/Pages/SearchDomainPage/SearchingDomain.jsx
@@ -5,6 +5,7 @@ import useDomain from "../../Hock/useDomain";
 import useCart from "../../Hock/useCart";
 import useAxiosPublic from "../../Hock/useAxiosPublic";
 import Swal from "sweetalert2";
+import LiveChat from "../../Component/Shared/liveChat section/LiveChat";
 
 
 const SearchingDomain = () => {
@@ -39,7 +40,7 @@ const SearchingDomain = () => {
                 description: domainItem?.description,
                 email: userInfo?.email,
                 review: "false",
-                payment:"false"
+                payment: "false"
             }
             const cartRes = await useAxios.post('/carts', cartItem)
             if (cartRes.data.acknowledged) {
@@ -54,63 +55,110 @@ const SearchingDomain = () => {
         }
     };
     return (
-        <div  className="  py-10  bg-firstColor dark:text-white px-[10%] px-auto">
-            <div className="mb-4  flex flex-col md:flex-row justify-center items-center pt-36 pb-20 dark:text-black">
-                <input
-                    type="text"
-                    placeholder="search here"
-                    className=" border md:w-full m-2 border-gray-300 rounded-lg mr-1 p-2 "
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <select
-                    className="md:p-2 mr-2 border border-gray-300 rounded-lg"
-                    value={selectedCategory}
-                    onChange={(e) => setSelectedCategory(e.target.value)}
-                >
-                    <option value="All">All Categories</option>
-                    <option value="technology">Technology</option>
-                    <option value="education">Education</option>
-                    <option value="commerce">Commerce</option>
-                    <option value="health">health</option>
-                    <option value="sport">Sport</option>
-                    <option value="industry">Industry</option>
-                    <option value="government">Government</option>
-                    {/* Add more categories as needed */}
-                </select>
-            </div>
-            <div className="flex gap-5">
-                <div className="flex flex-col gap-4 w-full p-2">
-                    {filteredDomains.length > 0 ? (
-                        // Your content when filteredDomains has items
-                        <p className="text-2xl md:text-3xl font-bold text-center text-white"> Available This Domain</p>
-                    ) : (
-                        // Your placeholder or fallback content
-                        <p className="text-2xl md:text-3x font-bold text-center text-white">Not available This Domain</p>
-                    )}
-                           <div  className="grid grid-cols-1 md:grid-cols-2 gap-5 ">  
-                    {filteredDomains.map((domainItem) => (
-               <div  key={domainItem?.id} className="relative flex justify-center w-full flex-col rounded-xl bg-fourthColor border-2 bg-clip-border p-2 text-white ">
-                   <div className="flex justify-between items-center">
-                       <h3 className="text-lg font-semibold">{searchTerm} {domainItem?.name}</h3>
-                       {/* <p className="text-gray-100">{domain.category}</p> */}
-                       <div className="flex justify-center items-center gap-5">
-                           <p className="text-gray-100">price: {domainItem.price}</p>
-                           <div className="border-2 p-2 bg-secondColor hover:bg-thirdColor rounded-lg">
-                               {/* Cart icon */}
-                               <MdAddShoppingCart className="  text-2xl cursor-pointer " onClick={() => addToCart(domainItem)} />
-                           </div>
-                       </div>
+        <>
+            <div className="w-full pt-16 flex flex-col-reverse md:flex-row justify-between items-center bg-thirdColor">
 
-                   </div>
+                <div className=" mx-5 lg:px-[10%] flex-grow">
+                    <p className="text-sm lg:text-xl text-center md:text-left text-white">SEARCH AVAILABLE DOMAIN NAMES</p>
 
-               </div>
-                    ))}
+                    <p className="text-4xl md:text-3xl lg:text-5xl text-center md:text-left text-white font-bold">Find your domain name</p>
+
+                    <p className="text-white text-sm lg:text-xl text-center md:text-left">Check if your domain’s available, browse alternatives, and land on one that’s perfect for your business, brand, or big idea.</p>
+
+
+                    <div className="mb-4  flex flex-col md:flex-row justify-center items-center lg:pt-5 dark:text-black">
+                        <input
+                            type="text"
+                            placeholder="search here"
+                            className=" border md:w-full m-2 border-gray-300 rounded-lg mr-1 p-2 "
+                            value={searchTerm}
+                            onChange={(e) => setSearchTerm(e.target.value)}
+                        />
+                        <select
+                            className="md:p-2 mr-2 border border-gray-300 rounded-lg"
+                            value={selectedCategory}
+                            onChange={(e) => setSelectedCategory(e.target.value)}
+                        >
+                            <option value="All">All Categories</option>
+                            <option value="technology">Technology</option>
+                            <option value="education">Education</option>
+                            <option value="commerce">Commerce</option>
+                            <option value="health">health</option>
+                            <option value="sport">Sport</option>
+                            <option value="industry">Industry</option>
+                            <option value="government">Government</option>
+                            {/* Add more categories as needed */}
+                        </select>
                     </div>
+
                 </div>
 
+
+                <div className="flex-grow">
+                    <img className="w-full h-full object-cover" src="https://i.postimg.cc/wBDkrpbZ/hero.png" alt="" />
+                </div>
             </div>
-        </div>
+
+            <div className="  py-10  bg-firstColor dark:bg-black dark:text-white lg:px-[10%] px-auto">
+                {/* <div className="mb-4  flex flex-col md:flex-row justify-center items-center pt-36 pb-20 dark:text-black">
+                    <input
+                        type="text"
+                        placeholder="search here"
+                        className=" border md:w-full m-2 border-gray-300 rounded-lg mr-1 p-2 "
+                        value={searchTerm}
+                        onChange={(e) => setSearchTerm(e.target.value)}
+                    />
+                    <select
+                        className="md:p-2 mr-2 border border-gray-300 rounded-lg"
+                        value={selectedCategory}
+                        onChange={(e) => setSelectedCategory(e.target.value)}
+                    >
+                        <option value="All">All Categories</option>
+                        <option value="technology">Technology</option>
+                        <option value="education">Education</option>
+                        <option value="commerce">Commerce</option>
+                        <option value="health">health</option>
+                        <option value="sport">Sport</option>
+                        <option value="industry">Industry</option>
+                        <option value="government">Government</option>
+                        
+                    </select>
+                </div> */}
+
+                <div className="flex gap-5">
+                    <div className="flex flex-col gap-4 w-full p-2">
+                        {filteredDomains.length > 0 ? (
+                            // Your content when filteredDomains has items
+                            <p className="text-2xl md:text-3xl font-bold text-center text-white"> Available This Domain</p>
+                        ) : (
+                            // Your placeholder or fallback content
+                            <p className="text-2xl md:text-3x font-bold text-center text-white">Not available This Domain</p>
+                        )}
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 ">
+                            {filteredDomains.map((domainItem) => (
+                                <div key={domainItem?.id} className="relative flex justify-center w-full flex-col rounded-xl bg-fourthColor border-2 bg-clip-border p-2 text-white ">
+                                    <div className="flex justify-between items-center">
+                                        <h3 className="text-lg font-semibold">{searchTerm} {domainItem?.name}</h3>
+                                        {/* <p className="text-gray-100">{domain.category}</p> */}
+                                        <div className="flex justify-center items-center gap-5">
+                                            <p className="text-gray-100">price: {domainItem.price}</p>
+                                            <div className="border-2 p-2 bg-secondColor hover:bg-thirdColor rounded-lg">
+                                                {/* Cart icon */}
+                                                <MdAddShoppingCart className="  text-2xl cursor-pointer " onClick={() => addToCart(domainItem)} />
+                                            </div>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <LiveChat></LiveChat>
+        </>
     );
 };
 export default SearchingDomain;

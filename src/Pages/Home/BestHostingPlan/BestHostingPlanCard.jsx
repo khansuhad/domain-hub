75bae993f802c3341d95183bf1e9b2d9bebe7b85
md monjur bakth mazumder <md.monjurmbm2001@gmail.com>
2024-02-19 01:33:55 +0600

    add funtonality in Chose The Best Hosting Plan

--- src/Pages/Home/BestHostingPlan/BestHostingPlanCard.jsx
+++ src/Pages/Home/BestHostingPlan/BestHostingPlanCard.jsx
@@ -4,17 +4,12 @@ import { useNavigate } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import { domainDetails } from "../../../features/domain/domainSlice";
 
-const OffersAndDiscountsCards = ({ data }) => {
+const BestHostingPlanCard = ({ data }) => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { t } = useTranslation();
   const handleDetails = () => {
-    const discountData = {
-      ...data,
-      price: (data.price * 0.8).toFixed(2),
-    };
-    console.log(discountData);
-    dispatch(domainDetails(discountData));
+    dispatch(domainDetails(data));
     navigate("/domainDetails");
   };
   return (
@@ -24,15 +19,12 @@ const OffersAndDiscountsCards = ({ data }) => {
     >
       <div className="card-body relative">
         <h2 className="text-3xl font-bold">{data?.name}</h2>
+        <p className="text-md italic  text-center underline my-1 py-px px-2 rounded bg-thirdColor text-white w-fit ">Total sold: {data?.sold}+</p>
         <p className="text-xl font-semibold">$Price</p>
-        <p className="text-md italic  text-center underline my-1 py-px px-2 rounded bg-thirdColor text-white w-fit ">
-          <del>{data?.price}</del> - 20% discount
+        <p className="text-5xl font-semibold flex items-center">
+          {data?.price}/<span className="text-lg">yearly</span>
         </p>
-        <p className="text-5xl font-semibold flex items-center my-3">
-          {(data?.price * 0.8).toFixed(2)}/
-          <span className="text-lg">yearly</span>
-        </p>
-        <div className="card-actions justify-center">
+        <div className="card-actions justify-center mt-3">
           <button
             className="block select-none rounded-lg bg-secondColor hover:bg-thirdColor border-b-2 p-2 text-center align-middle font-sans text-sm font-semibold uppercase text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none mb-5"
             type="button"
@@ -47,8 +39,8 @@ const OffersAndDiscountsCards = ({ data }) => {
   );
 };
 
-OffersAndDiscountsCards.propTypes = {
+BestHostingPlanCard.propTypes = {
   data: PropTypes.object,
 };
 
-export default OffersAndDiscountsCards;
+export default BestHostingPlanCard;
--- src/Pages/Home/PriceByCategory/CategoryDetails.jsx
+++ src/Pages/Home/PriceByCategory/CategoryDetails.jsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import img from "../../../assets/bannerImg/domain_pix.png"
+import img from "../../../assets/bannerImg/domain_pix.png";
 import { useState } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
 import useAxiosPublic from "../../../Hock/useAxiosPublic";
@@ -10,102 +10,144 @@ import useAllSoldDomain from "../../../Hock/useAllSoldDomain";
 import { useTranslation } from "react-i18next";
 
 const CategoryDetails = () => {
-    const { t } = useTranslation()
-    const [searchedDomain, setSearchedDomain] = useState("")
-    const [notAvailable, setNotAvailable] = useState("")
-    const navigate = useNavigate()
-    const userInfo = useSelector((state) => state.user.currentUser);
-    const domainDetails = useSelector((state) => state.domain.domain)
-    const axiosSecure = useAxiosPublic()
-    const [carts,loading, refetch]= useCart()
-    const [alldomain] = useAllSoldDomain()
-    console.log("cart item:", alldomain);
-    const bookedDomains = alldomain?.filter(item => item.payment === "true")
-    const handleSearch = (e) => {
-        e.preventDefault()
-        const form = e.target
-        const searchData = form.domain.value
-        const domainSearched = searchData.concat(domainDetails?.name)
-        const domain= domainSearched.toLowerCase()
-        console.log("lowercase domain",domain);
-        const findDomain = bookedDomains.filter(item => item.name === domain)
-        console.log("test status", findDomain?.length);
-        if (findDomain?.length > 0) {
-            setNotAvailable("Sorry! this domain is already taken.")
-            setSearchedDomain("")
-        }
-        else {
-            setSearchedDomain(domain)
-            setNotAvailable("")
-        }
+  const { t } = useTranslation();
+  const [searchedDomain, setSearchedDomain] = useState("");
+  const [notAvailable, setNotAvailable] = useState("");
+  const navigate = useNavigate();
+  const userInfo = useSelector((state) => state.user.currentUser);
+  const domainDetails = useSelector((state) => state.domain.domain);
+  const axiosSecure = useAxiosPublic();
+  const [carts, loading, refetch] = useCart();
+  const [alldomain] = useAllSoldDomain();
+  console.log("cart item:", alldomain);
+  const bookedDomains = alldomain?.filter((item) => item.payment === "true");
+  const handleSearch = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const searchData = form.domain.value;
+    const domainSearched = searchData.concat(domainDetails?.name);
+    const domain = domainSearched.toLowerCase();
+    console.log("lowercase domain", domain);
+    const findDomain = bookedDomains.filter((item) => item.name === domain);
+    console.log("test status", findDomain?.length);
+    if (findDomain?.length > 0) {
+      setNotAvailable("Sorry! this domain is already taken.");
+      setSearchedDomain("");
+    } else {
+      setSearchedDomain(domain);
+      setNotAvailable("");
     }
-    console.log("domain searched",searchedDomain);
-    const cartItem = {
-        name: searchedDomain,
-        category: domainDetails?.category,
-        price: domainDetails?.price,
-        description: domainDetails?.description,
-        email: userInfo?.email,
-        review: "false",
-        payment:"false"
-    }
-    const handleCart = () => {
-        axiosSecure.post("/carts", cartItem)
-            .then(res => {
-                if (res.data.insertedId) {
-                    console.log(res.data);
-                    toast.success("Successfully Added to Cart!")
-                    refetch()
-                    navigate("/dashboard/myCart")
-                    
-                }
-            }
-            )
-    }
-    return (
-        <div className="bg-firstColor ">
-            <div className="w-[95%] lg:w-[90%] mx-auto dark:text-white pt-32 ">
-            <div className="flex flex-col md:flex-row justify-around items-center pt-24 pb-16  dark:text-black bg-fourthColor rounded-xl">
-                <div className="space-y-5 pl-5">
-                    <p className="text-xl font-semibold text-white"> <span className="bg-blue-700 text-white p-1 rounded ">{domainDetails?.name}</span>  Domain Names</p>
-                    <p className="text-4xl font-bold text-white">{domainDetails?.description}</p>
-                    <p className="text-xl font-semibold text-white">Available now! Starting at</p>
-                    <p className="text-4xl font-semibold bg-blue-700 text-white p-1 rounded w-fit">${domainDetails?.price}/yr</p>
-                </div>
-                <div>
-                    <img src={img} alt="" />
-                </div>
-            </div>
-            <div className="lg:mx-28 pt-5">
-                <p className="lg:text-2xl text-center  my-3 font-bold text-white ">Start your {domainDetails?.name} domain search now.</p>
-                <div >
-                    <form action="" onSubmit={handleSearch} className="flex justify-center gap-2">
-                        <div className="relative w-full max-w-2xl">
-                            <input type="text" name="domain" placeholder={t("domainEnter")} className="w-full text-black max-w-2xl p-3 rounded-lg outline-info border-2" />
-                            <p className="p-3 bg-blue-700 text-white absolute right-0 top-0 rounded-r-lg border-slate-700 border-2">{domainDetails?.name}</p>
-                        </div>
+  };
+  console.log(" domainDetails", domainDetails);
+  console.log("domain searched", searchedDomain);
+  const cartItem = {
+    name: searchedDomain,
+    category: domainDetails?.category,
+    price: domainDetails?.price,
+    description: domainDetails?.description,
+    email: userInfo?.email,
+    review: "false",
+    payment: "false",
+    domainId: domainDetails?._id,
+  };
+  const handleCart = () => {
+    axiosSecure.post("/carts", cartItem).then((res) => {
+      if (res.data.insertedId) {
+        console.log(res.data);
+        toast.success("Successfully Added to Cart!");
+        refetch();
+        navigate("/dashboard/myCart");
+      }
+    });
+  };
+  return (
+    <div className="bg-firstColor ">
+      <div className="w-[95%] lg:w-[90%] mx-auto dark:text-white pt-32 ">
+        <div className="flex flex-col md:flex-row justify-around items-center pt-24 pb-16  dark:text-black bg-fourthColor rounded-xl">
+          <div className="space-y-5 pl-5">
+            <p className="text-xl font-semibold text-white">
+              {" "}
+              <span className="bg-blue-700 text-white p-1 rounded ">
+                {domainDetails?.name}
+              </span>{" "}
+              Domain Names
+            </p>
+            <p className="text-4xl font-bold text-white">
+              {domainDetails?.description}
+            </p>
+            <p className="text-xl font-semibold text-white">
+              Available now! Starting at
+            </p>
+            <p className="text-4xl font-semibold bg-blue-700 text-white p-1 rounded w-fit">
+              ${domainDetails?.price}/yr
+            </p>
+          </div>
+          <div>
+            <img src={img} alt="" />
+          </div>
+        </div>
+        <div className="lg:mx-28 pt-5">
+          <p className="lg:text-2xl text-center  my-3 font-bold text-white ">
+            Start your {domainDetails?.name} domain search now.
+          </p>
+          <div>
+            <form
+              action=""
+              onSubmit={handleSearch}
+              className="flex justify-center gap-2"
+            >
+              <div className="relative w-full max-w-2xl">
+                <input
+                  type="text"
+                  name="domain"
+                  placeholder={t("domainEnter")}
+                  className="w-full text-black max-w-2xl p-3 rounded-lg outline-info border-2"
+                />
+                <p className="p-3 bg-blue-700 text-white absolute right-0 top-0 rounded-r-lg border-slate-700 border-2">
+                  {domainDetails?.name}
+                </p>
+              </div>
 
-                        <button type="submit" className="block select-none rounded-lg bg-secondColor hover:bg-thirdColor border-b-2 py-2 px-4 text-center align-middle font-sans text-sm font-semibold uppercase text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none">{t("bannerSearchBtn")}</button>
-                    </form>
-                    <div>
-                        {searchedDomain?.length > 0 ?
-                            <div className="text-white py-5">
-                                <p className="text-center py-4 text-xl font-semibold">{t("domainAvailable")}</p>
-                                <div className="relative flex justify-center w-full flex-col rounded-xl bg-fourthColor border-2 bg-clip-border p-2 text-white ">
-                                    <div className="flex justify-between">
-                                        <p>{searchedDomain}</p>
-                                        <div className="flex gap-1 cursor-pointer" onClick={handleCart} > <span>{t("addToCart")} </span> <MdAddShoppingCart className="  text-2xl " /></div>
-                                    </div>
-                                </div>
-                            </div> : <div>
-                                <p className="text-xl text-center pt-5 font-semibold text-red-600">{notAvailable}</p>
-                            </div>}
+              <button
+                type="submit"
+                className="block select-none rounded-lg bg-secondColor hover:bg-thirdColor border-b-2 py-2 px-4 text-center align-middle font-sans text-sm font-semibold uppercase text-white shadow-md shadow-blue-gray-500/10 transition-all hover:scale-[1.02] hover:shadow-lg hover:shadow-blue-gray-500/20 focus:scale-[1.02] focus:opacity-[0.85] focus:shadow-none active:scale-100 active:opacity-[0.85] active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              >
+                {t("bannerSearchBtn")}
+              </button>
+            </form>
+            <div>
+              {searchedDomain?.length > 0 ? (
+                <div className="text-white py-5">
+                  <p className="text-center py-4 text-xl font-semibold">
+                    {t("domainAvailable")}
+                  </p>
+                  <div className="relative flex justify-center w-full flex-col rounded-xl bg-fourthColor border-2 bg-clip-border p-2 text-white ">
+                    <div className="flex justify-between">
+                      <p>{searchedDomain}</p>
+                      <div
+                        className="flex gap-1 cursor-pointer"
+                        onClick={handleCart}
+                      >
+                        {" "}
+                        <span>{t("addToCart")} </span>{" "}
+                        <MdAddShoppingCart className="  text-2xl " />
+                      </div>
                     </div>
+                  </div>
                 </div>
+              ) : (
+                <div>
+                  <p className="text-xl text-center pt-5 font-semibold text-red-600">
+                    {notAvailable}
+                  </p>
+                </div>
+              )}
             </div>
+          </div>
         </div>
-        </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default CategoryDetails;

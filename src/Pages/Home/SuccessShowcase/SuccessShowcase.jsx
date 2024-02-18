15741b0ae435b637138f926197c527d1786c75ed
Digontha <digonthodj@gmail.com>
2024-02-18 14:42:50 +0600

    implement some animation in freetrial and showcase section

--- src/Pages/Home/SuccessShowcase/SuccessShowcase.jsx
+++ src/Pages/Home/SuccessShowcase/SuccessShowcase.jsx
@@ -15,45 +15,48 @@ const SuccessShowcase = () => {
   const [sellings, setSellings] = useState([])
   const [reviews, setReviews] = useState([])
   const [users, setUsers] = useState([])
-  const {t}=useTranslation()
+  const { t } = useTranslation()
   const useAxios = useAxiosPublic();
 
-  
-  useEffect(() =>{
-    useAxios.get("/reviewsLength")
-    .then(res => {
-      const data = res?.data ;
-      setReviews(data)
-    })
-    useAxios.get("/usersLength")
-    .then(res => {
-      const data = res?.data;
-      setUsers(data)
-    })
-    useAxios.get("/sellingsLength")
-    .then(res => {
-      const data = res?.data;
-      setSellings(data)
-    })
 
-  },[useAxios])
+  useEffect(() => {
+    useAxios.get("/reviewsLength")
+      .then(res => {
+        const data = res?.data;
+        setReviews(data)
+      })
+    useAxios.get("/usersLength")
+      .then(res => {
+        const data = res?.data;
+        setUsers(data)
+      })
+    useAxios.get("/sellingsLength")
+      .then(res => {
+        const data = res?.data;
+        setSellings(data)
+      })
+
+  }, [useAxios])
 
   return (
     <Container>
       <Heading>{t("successTitle")}</Heading>
       <Description>
-       {t("successDes")}
+        {t("successDes")}
       </Description>
       <div className="w-full flex justify-center items-center mt-16 mb-16">
         <div className="max-w-7xl mx-auto w-full grid grid-cols-9">
           {/* stack-1 */}
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+              data-aos="flip-left"
+              data-aos-easing="ease-out-cubic"
+              data-aos-duration="2000">
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
                 {t("success1Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success1Title")}
+                {t("success1Title")}
               </p>
             </div>
           </div>
@@ -76,12 +79,16 @@ const SuccessShowcase = () => {
           </div>
 
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+            data-aos="flip-right"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            >
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
-              {t("success2Title")}
+                {t("success2Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success2Des")}
+                {t("success2Des")}
               </p>
             </div>
           </div>
@@ -89,12 +96,16 @@ const SuccessShowcase = () => {
 
           {/* stack-3 */}
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            >
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
-              {t("success3Title")}
+                {t("success3Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success3Des")}
+                {t("success3Des")}
               </p>
             </div>
           </div>
@@ -117,12 +128,16 @@ const SuccessShowcase = () => {
           </div>
 
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+            data-aos="flip-right"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            >
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
-              {t("success3Title")}
+                {t("success3Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success3Des")}
+                {t("success3Des")}
               </p>
             </div>
           </div>
@@ -130,12 +145,16 @@ const SuccessShowcase = () => {
 
           {/* stack-5 */}
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            >
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
-              {t("success4Title")}
+                {t("success4Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success4Des")}
+                {t("success4Des")}
               </p>
             </div>
           </div>
@@ -158,12 +177,16 @@ const SuccessShowcase = () => {
           </div>
 
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+            data-aos="flip-right"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"        
+            >
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
-              {t("success5Title")}
+                {t("success5Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success5Des")}
+                {t("success5Des")}
               </p>
             </div>
           </div>
@@ -171,12 +194,16 @@ const SuccessShowcase = () => {
 
           {/* stack-7 */}
           <div className="col-span-4 w-full h-full">
-            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2">
+            <div className="w-full h-full bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] rounded-md p-2"
+            data-aos="flip-left"
+            data-aos-easing="ease-out-cubic"
+            data-aos-duration="2000"
+            >
               <h1 className="text-white text-xl font-medium py-2 md:pl-4">
-              {t("success6Title")}
+                {t("success6Title")}
               </h1>
               <p className="text-gray-100 sm:text-sm text-xs p-1">
-              {t("success6Des")}
+                {t("success6Des")}
               </p>
             </div>
           </div>
@@ -191,42 +218,42 @@ const SuccessShowcase = () => {
         </div>
       </div>
       <div className="flex flex-col md:flex-row lg:flex-row justify-center items-center gap-5 md:gap-10 lg:gap-24 mt-20">
-        <div className="stats shadow">
-          <div className="stat bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] text-white">
+        <div className="stats shadow" data-aos="flip-up">
+          <div className="stat bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] text-white"  >
             <div className="flex justify-between gap-3">
               <div>
                 <div className="stat-title font-bold text-white">
                   {t("totalSell")}
                 </div>
                 <div className="stat-value">
-               <CountUp end={sellings?.length}/>
+                  <CountUp end={sellings?.length} />
                 </div>
               </div>
               <MdSell className="mt-6 text-xl" />
             </div>
           </div>
         </div>
-        <div className="stats shadow">
+        <div className="stats shadow"  data-aos="flip-up">
           <div className="stat bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] text-white">
             <div className="flex justify-between gap-3">
               <div>
-                <div className="stat-title font-bold text-white">
-                {t("totalUser")}
+                <div className="stat-title font-bold text-white" >
+                  {t("totalUser")}
                 </div>
                 <div className="stat-value">
-                  <CountUp end={users?.length}/>
+                  <CountUp end={users?.length} />
                 </div>
               </div>
               <FaUserGroup className="mt-6 text-xl" />
             </div>
           </div>
         </div>
-        <div className="stats shadow">
-          <div className="stat bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] text-white">
+        <div className="stats shadow" data-aos="flip-up">
+          <div className="stat bg-gradient-to-tr from-[#ac50ef] via-[#7059fb] to-[#23668a] text-white"  >
             <div className="flex justify-between gap-3">
               <div>
                 <div className="stat-title font-bold text-white">
-                {t("totalReview")}
+                  {t("totalReview")}
                 </div>
                 <div className="stat-value">
                   <CountUp end={reviews?.length} />

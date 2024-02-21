34602e38214379dc886566b6efe7841a9d5f4600
Digontha <digonthodj@gmail.com>
2024-02-21 14:33:48 +0600

    Format

--- src/Pages/Dashboard/MyFreeTailApplication/MyFreeTailApplication.jsx
+++ src/Pages/Dashboard/MyFreeTailApplication/MyFreeTailApplication.jsx
@@ -2,49 +2,17 @@
 
 
 import useSingleFreeTrialUser from "../../../Hock/useSingleFreeTrialUser";
-import { MdDelete } from "react-icons/md";
-import { IoDiamond } from "react-icons/io5";
-import useAxiosPublic from "../../../Hock/useAxiosPublic";
-import Swal from "sweetalert2";
+import { IoDiamond } from "react-icons/io5";;
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { domainDetails } from "../../../features/domain/domainSlice";
 import useDomain from "../../../Hock/useDomain";
 
+
 const MyFreeTailApplication = () => {
     const [singleFreeTrialUser, , refetch] = useSingleFreeTrialUser()
     console.log(singleFreeTrialUser);
-    const axiosPublic = useAxiosPublic()
 
-    const handleDelete = (email) => {
-        Swal.fire({
-            title: "Are you sure?",
-            text: "You won't be able to revert this!",
-            icon: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#3085d6",
-            cancelButtonColor: "#d33",
-            confirmButtonText: "Yes, delete it!"
-        }).then(async (res) => {
-            if (res.isConfirmed) {
-                axiosPublic.delete(`/freeTrialUsers?email=${email}`)
-                    .then(res => {
-                        console.log(res.data);
-
-                        if (res.data.deletedCount > 0) {
-                            Swal.fire({
-                                title: "Deleted!",
-                                text: " Item has been deleted",
-                                icon: "success"
-                            });
-                            refetch();
-                        }
-                    })
-            }
-        });
-
-
-    };
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
@@ -99,11 +67,7 @@ const MyFreeTailApplication = () => {
                                     Application
                                 </p>
                             </th>
-                            <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                                <p className="block font-sans text-sm lg:text-xl font-bold antialiased leading-none text-white opacity-70">
-                                    Delete
-                                </p>
-                            </th>
+
 
                             {singleFreeTrialUser.map(item => <>
                                 {
@@ -125,27 +89,27 @@ const MyFreeTailApplication = () => {
                         singleFreeTrialUser.map((users, inx) => <>
                             <tbody className="text-white">
                                 <tr>
-                                    <td className="p-4 border-b border-blue-gray-50">
+                                    <td className="p-4 ">
                                         <p className="block font-sans text-sm lg:text-xl  antialiased font-normal leading-normal text-blue-gray-900">
                                             {inx + 1}
                                         </p>
                                     </td>
-                                    <td className="p-4 border-b border-blue-gray-50">
+                                    <td className="p-4 ">
                                         <p className="block font-sans text-sm lg:text-xl  antialiased font-normal leading-normal text-blue-gray-900">
                                             {users.userName}
                                         </p>
                                     </td>
-                                    <td className="p-4 border-b border-blue-gray-50">
+                                    <td className="p-4 ">
                                         <p className="block font-sans text-sm lg:text-xl  antialiased font-normal leading-normal text-blue-gray-900">
                                             {users?.email}
                                         </p>
                                     </td>
-                                    <td className="p-4 border-b border-blue-gray-50">
+                                    <td className="p-4 ">
                                         <p className="block font-sans text-sm lg:text-xl  antialiased font-normal leading-normal text-blue-gray-900">
                                             {users?.domainName}
                                         </p>
                                     </td>
-                                    <td className="p-4 border-b border-blue-gray-50 flex items-center gap-3">
+                                    <td className="p-4  flex items-center gap-3">
                                         {users?.approve == "Accepted" && <p className="block cursor-pointer text-black text-center lg:text-xl lg:font-semibold bg-green-400 p-2 rounded-sm font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
                                             Accepted
                                         </p>}
@@ -162,12 +126,6 @@ const MyFreeTailApplication = () => {
 
                                     </td>
 
-                                    <td className="p-4 border-b border-blue-gray-50">
-                                        <button onClick={() => { handleDelete(users?.email) }} className="text-2xl text-red-500"><MdDelete></MdDelete></button>
-                                    </td>
-
-
-
                                     {users?.claimDomain ?
                                         <td className="p-4 border-b border-blue-gray-50  items-center gap-3">
                                             <p data-ripple-dark="true" className="flex items-center gap-3 lg:w-[50%] cursor-pointer text-black text-center lg:text-xl lg:font-semibold bg-secondColor p-2 rounded-sm   font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
@@ -181,10 +139,6 @@ const MyFreeTailApplication = () => {
                                             </p>}
                                         </td>
                                     }
-
-
-
-
 
                                 </tr>
 

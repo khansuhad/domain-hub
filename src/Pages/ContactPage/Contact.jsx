3a07cd02e34a2d7d2899ab6624f4c0f1a0c06f36
Fahim-M-Siam <fahim.mohammad.siam@gmail.com>
2024-02-23 20:09:08 +0600

    Implmented React-Helmet in Contact us page

--- src/Pages/ContactPage/Contact.jsx
+++ src/Pages/ContactPage/Contact.jsx
@@ -7,6 +7,7 @@ import Swal from "sweetalert2";
 // import Container from "../../Component/UI/Container";
 import Heading from "../../Component/UI/Heading";
 import { useTranslation } from "react-i18next";
+import { Helmet } from "react-helmet";
 // import { useSelector } from "react-redux";
 
 const Contact = () => {
@@ -45,102 +46,109 @@ const Contact = () => {
   };
 
   return (
-    <div
-      id="contact"
-      className=" px-2  lg:px-[10%] bg-firstColor dark:bg-[#191919] dark:text-white text-white py-10"
-    >
-      <h2 className="text-2xl md:text-3xl font-bold text-center my-20">
-        <Heading>{t("contactUs")}</Heading>
-      </h2>
-      <div className="flex flex-col md:flex-row justify-center gap-10">
-        <div className=" text-center">
-          <div className="text-3xl text-sky-500 flex justify-center">
-            <IoCall />
+    <>
+      <Helmet>
+        <title>DomainHub | Contact</title>
+        <meta name="description" content="Helmet application" />
+      </Helmet>
+
+      <div
+        id="contact"
+        className=" px-2  lg:px-[10%] bg-firstColor dark:bg-[#191919] dark:text-white text-white py-10"
+      >
+        <h2 className="text-2xl md:text-3xl font-bold text-center my-20">
+          <Heading>{t("contactUs")}</Heading>
+        </h2>
+        <div className="flex flex-col md:flex-row justify-center gap-10">
+          <div className=" text-center">
+            <div className="text-3xl text-sky-500 flex justify-center">
+              <IoCall />
+            </div>
+            <p className="text-2xl font-bold">{t("call")}</p>
+            +8801728306504
           </div>
-          <p className="text-2xl font-bold">{t("call")}</p>
-          +8801728306504
+
+          <div className=" text-center">
+            <div className="text-3xl flex justify-center text-sky-500">
+              <MdEmail />
+            </div>
+            <p className="text-2xl font-bold">{t("contactEmailTitle")}</p>
+            <p>domainhub321@gmail.com</p>
+          </div>
+          <div className=" text-center">
+            <div className="text-3xl text-center text-sky-500 flex justify-center">
+              {" "}
+              <FaLocationDot />
+            </div>
+            <p className="text-2xl font-bold">{t("location")}</p>
+            <p>Brahmanbaria,Bangladesh</p>
+          </div>
         </div>
 
-        <div className=" text-center">
-          <div className="text-3xl flex justify-center text-sky-500">
-            <MdEmail />
-          </div>
-          <p className="text-2xl font-bold">{t("contactEmailTitle")}</p>
-          <p>domainhub321@gmail.com</p>
-        </div>
-        <div className=" text-center">
-          <div className="text-3xl text-center text-sky-500 flex justify-center">
-            {" "}
-            <FaLocationDot />
-          </div>
-          <p className="text-2xl font-bold">{t("location")}</p>
-          <p>Brahmanbaria,Bangladesh</p>
-        </div>
-      </div>
+        <div className="border border-secondColor  py-5 lg:m-8 my-4 rounded-xl bg-fourthColor bg-clip-border p-8 text-white shadow-md shadow-black">
+          <form ref={form} className="card-body" onSubmit={sendEmail}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold text-white">
+                  {t("contactNameTitle")}
+                </span>
+              </label>
+              <input
+                type="text"
+                placeholder={t("contactName")}
+                name="from_name"
+                className="input border input-bordered text-black"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold text-white">
+                  {t("contactEmailTitle")}
+                </span>
+              </label>
+              <input
+                type="email"
+                placeholder={t("contactEmail")}
+                name="from_email"
+                className="input input-bordered text-black"
+                required
+              />
+            </div>
 
-      <div className="border border-secondColor  py-5 lg:m-8 my-4 rounded-xl bg-fourthColor bg-clip-border p-8 text-white shadow-md shadow-black">
-        <form ref={form} className="card-body" onSubmit={sendEmail}>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text font-bold text-white">
-                {t("contactNameTitle")}
-              </span>
-            </label>
-            <input
-              type="text"
-              placeholder={t("contactName")}
-              name="from_name"
-              className="input border input-bordered text-black"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text font-bold text-white">
-                {t("contactEmailTitle")}
-              </span>
-            </label>
-            <input
-              type="email"
-              placeholder={t("contactEmail")}
-              name="from_email"
-              className="input input-bordered text-black"
-              required
-            />
-          </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text font-bold text-white">
+                  {t("messageTitle")}
+                </span>
+              </label>
+              <textarea
+                className="border rounded p-5 text-black"
+                name="message"
+                placeholder={t("message")}
+              />
 
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text font-bold text-white">
-                {t("messageTitle")}
-              </span>
-            </label>
-            <textarea
-              className="border rounded p-5 text-black"
-              name="message"
-              placeholder={t("message")}
-            />
+              <input
+                className="btn mt-5 uppercase font-bold text-xl text-white bg-secondColor hover:bg-thirdColor "
+                type="submit"
+                value={t("send")}
+              />
+            </div>
 
-            <input
-              className="btn mt-5 uppercase font-bold text-xl text-white bg-secondColor hover:bg-thirdColor "
-              type="submit"
-              value={t("send")}
-            />
-          </div>
-
-          {/* <label>Name</label> 
+            {/* <label>Name</label> 
                     <input type="text" name="from_name" /> */}
 
-          {/* <label>Email</label>
+            {/* <label>Email</label>
                     <input type="email" name="from_email" /> */}
 
-          {/* <label>Message</label>
+            {/* <label>Message</label>
                     <textarea name="message" />
 
                     <input type="submit" value="Send" /> */}
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
